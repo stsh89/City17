436a02da8677bfbc9a1a51fdeeafd4315185fcf7
Stanislav Shandyga <stanislavshandyga@gmail.com>
2025-05-23 09:01:49 +0300

    Add SQL query to create symbol

--- internal-crates/catacombs17-storage/src/wisdom/symbols.rs
+++ internal-crates/catacombs17-storage/src/wisdom/symbols.rs
@@ -1,1 +1,34 @@
-pub struct Symbol;
+use chrono::{DateTime, Utc};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+pub struct Symbol {
+    pub id: Uuid,
+    pub title: String,
+    pub formula: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+pub struct NewSymbol {
+    pub title: String,
+    pub formula: String,
+}
+
+pub async fn create_symbol(pool: &PgPool, symbol: NewSymbol) -> sqlx::Result<Symbol> {
+    let NewSymbol { title, formula } = symbol;
+
+    sqlx::query_as!(
+        Symbol,
+        "
+INSERT INTO wisdom.symbols (id, title, formula)
+VALUES ($1, $2, $3)
+RETURNING id, title, formula, created_at, updated_at
+        ",
+        Uuid::now_v7(),
+        title,
+        formula
+    )
+    .fetch_one(pool)
+    .await
+}
